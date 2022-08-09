f5d5d884e91ad75ab61e72dc023b3dbbd98f3245
DevNeoLee <devneolee@gmail.com>
2022-08-08 18:25:03 -0700

    Change to new URL

--- frontend/src/utils/routes.js
+++ frontend/src/utils/routes.js
@@ -1,6 +1,6 @@
 let HOST;
 if (process.env.NODE_ENV === 'production') {
-    HOST = 'https://mern-12-chatting-feature.herokuapp.com'
+    HOST = 'https://ufokn-game.herokuapp.com'
 } else {
     HOST = 'http://localhost:5000'
 }
